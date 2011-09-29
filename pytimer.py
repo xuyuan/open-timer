25a03c84d770d6ee83217c5b9ef1bf2e2c2fffa2
Xu Yuan <xu@informatik.hu-berlin.de>
2011-09-29 23:16:48 +0200

    adjust arguments

--- pytimer.py
+++ pytimer.py
@@ -15,7 +15,7 @@ class PyTimerDaemon(Daemon):
 		tray.main()
 
 def usage():
-	print "usage: %s start|stop|restart|status" % sys.argv[0]
+	print "usage: %s start|stop|restart|status" % ""#sys.argv[0]
 
 if __name__ == "__main__":
 	daemon = PyTimerDaemon('/tmp/pytimer-daemon.pid', stderr='/tmp/pytimer-stderr.log', stdout='/tmp/pytimer-stdout.log')
@@ -27,8 +27,6 @@ if __name__ == "__main__":
 			usage()
 			sys.exit(2)
 		sys.exit(0)
-	elif len(sys.argv) == 1:
-		daemon.start()
 	else:
 		usage()
 		sys.exit(2)
